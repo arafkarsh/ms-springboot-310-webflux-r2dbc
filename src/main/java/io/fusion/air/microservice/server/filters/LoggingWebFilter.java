5736defdb06a7b87ae542ff761d65b5bcb84ee42
Araf Karsh Hamid <araf.karsh@metamagic.in>
2023-08-30 10:40:36 +0530

    Propagating Exceptions From Handler to Global Handler

--- src/main/java/io/fusion/air/microservice/server/filters/LoggingWebFilter.java
+++ src/main/java/io/fusion/air/microservice/server/filters/LoggingWebFilter.java
@@ -17,21 +17,15 @@ package io.fusion.air.microservice.server.filters;
 
 import io.fusion.air.microservice.server.config.ServiceConfiguration;
 import io.fusion.air.microservice.server.models.RequestMap;
-import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.result.view.RequestContext;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 import reactor.util.context.Context;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 /**
  * @author: Araf Karsh Hamid
@@ -63,5 +57,4 @@ public class LoggingWebFilter implements WebFilter {
                 .doOnEach(signal -> {
                 });
     }
-
 }
